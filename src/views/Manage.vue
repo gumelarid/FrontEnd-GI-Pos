d954017c5d529f2d4107099f1c921d7b577ec31d
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-08-24 19:28:51 +0700

    feat: method add data product

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -15,10 +15,13 @@
     <Sidebar />
     <b-container fluid style="padding-left:0; padding-right:0;">
       <b-row class="main">
-        <b-col md="12" class="items">
-          <h4>All Product</h4>
+        <b-col md="12">
           <b-container>
+            <h3 class="items">All Product</h3>
+            <b-button v-b-modal.modal-add>Add Product</b-button>
+            <hr />
             <b-table
+              class="items"
               id="table-transition-example"
               :items="items"
               :fields="fields"
@@ -33,6 +36,57 @@
               </template>
             </b-table>
           </b-container>
+          <b-modal id="modal-add" ref="modal-add" centered title="BootstrapVue" hide-footer>
+            <form @submit.prevent="addProduct">
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    v-model="form.product_name"
+                    class="form-control"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Image</label>
+                <div class="col-sm-10">
+                  <input type="text" v-model="form.product_image" class="form-control" required />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Price</label>
+                <div class="col-sm-10">
+                  <input
+                    type="number"
+                    v-model="form.product_price"
+                    class="form-control price"
+                    placeholder="Price"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Category</label>
+                <div class="col-sm-10">
+                  <select class="custom-select" v-model="form.id_category" required>
+                    <option disabled value>Please select Category</option>
+                    <option
+                      v-bind:value="value.category_id"
+                      v-for="(value, index) in category"
+                      :key="index"
+                    >{{value.category_name}}</option>
+                  </select>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="reset" class="btn btn-cancel">reset</button>
+                <button type="submit" class="btn btn-add">Add</button>
+              </div>
+            </form>
+          </b-modal>
         </b-col>
       </b-row>
     </b-container>
@@ -50,16 +104,26 @@ export default {
   },
   data() {
     return {
+      alert: false,
+      isMsg: '',
       total_data: 0,
       perPage: 7,
       currentPage: 2,
-      items: [],
+      category: [],
+      product: [],
       fields: [
         { key: 'product_name', sortable: true },
         { key: 'category_name', sortable: true },
         { key: 'product_price', sortable: true },
         { key: 'actions', label: 'Actions' }
-      ]
+      ],
+      form: {
+        id_category: '',
+        product_name: '',
+        product_image: '',
+        product_price: '',
+        status: 1
+      }
     }
   },
   computed: {
@@ -68,21 +132,50 @@ export default {
     }
   },
   created() {
-    this.getProduct()
+    // this.getProduct()
+    this.getCategory()
   },
   methods: {
-    getProduct() {
+    makeToast(msg, append = false) {
+      this.$bvToast.toast(`${msg}`, {
+        title: 'Success',
+        autoHideDelay: 10000,
+        appendToast: append
+      })
+    },
+    getCategory() {
       axios
-        .get(
-          `http://127.0.0.1:3001/product?limit=${this.perPage}?page=${this.currentPage}`
-        )
+        .get('http://127.0.0.1:3001/category')
         .then((response) => {
-          this.items = response.data.data[0]
-          this.total_data = response.data.data[1].totalData
-          console.log(response.data.data[1].totalData)
+          this.category = response.data.data
         })
         .catch((error) => {
           console.log(error)
+        })
+    },
+    getProduct() {
+      axios
+        .get(`http://127.0.0.1:3001/product?limit=${this.perPage}`)
+        .then((response) => {
+          this.product = response.data.data[0]
+          this.total_data = response.data.data[1].totalData
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    closeModal() {
+      this.$refs['modal-add'].hide()
+    },
+    addProduct() {
+      axios
+        .post('http://127.0.0.1:3001/product', this.form)
+        .then((response) => {
+          this.products = response.data.data
+          this.isMsg = response.data.msg
+          this.makeToast(this.isMsg)
+          this.closeModal()
+          this.getProduct()
         })
     }
   }
@@ -133,5 +226,41 @@ export default {
   margin-left: 0;
   margin-right: 0;
   padding: 5px 2px;
+}
+.modal-body form input {
+  border: 1px solid #cecece;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+}
+
+.modal-body .price {
+  width: 270px;
+  border: 1px solid #cecece;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+}
+
+.modal-body form select {
+  width: 200px;
+  border: 1px solid #cecece;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+}
+
+.btn-add {
+  width: 80px;
+  border-radius: 100px;
+  background-color: #57cad5;
+  color: #ffffff;
+}
+
+.btn-cancel {
+  width: 80px;
+  border-radius: 100px;
+  background-color: #f24f8a;
+  color: #ffffff;
 }
 </style>
